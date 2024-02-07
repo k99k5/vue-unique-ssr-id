28837d5a9f547d92c174b92545b224a2b12ec1bf
k99k5 <k99k5@qq.com>
2024-02-07 09:30:00 +0800

    feat: 0.0.3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import {getCurrentInstance} from "vue";
+import {createCRC32} from "hash-wasm";
 
 /**
  * @param app {{directive,mixin}} Vue实例
  */
 export function vueBindSSRPlugin(app) {
-    app.directive('v-ssr', {});
+    app.directive('ssr', {});
     app.mixin({
         props: {
             "dataSsrId": {
@@ -15,51 +16,46 @@ export function vueBindSSRPlugin(app) {
     })
 }
 
-import {crc32} from "hash-wasm";
-export const viteBindSSRPlugin = {
-    name: 'bind-ssr-id',
-    transform(src, id) {
-        if (!/\.(vue)$/.test(id)) {
-            return null;
-        }
-        
-        //首先查找v-ssr所有的位置
-        let ssrIndex = [];
-        let index = src.indexOf('v-ssr');
-        while (index !== -1) {
-            ssrIndex.push(index);
-            index = src.indexOf('v-ssr', index + 1);
-        }
-        
-        //替换v-ssr为data-ssr-id=${hash(file+line+column)}
-        let result = src;
-        let task = Promise.resolve();
-        for (let index of ssrIndex) {
-            let line = 1;
-            let column = 0;
-            for (let i = 0; i < index; i++) {
-                if (src[i] === '\n') {
-                    line++;
-                    column = 0;
-                } else {
-                    column++;
-                }
+export async function viteBindSSRPlugin() {
+    const crc32Instance = (await createCRC32());
+    return {
+        name: 'bind-ssr-id',
+        transform(src, id) {
+            if (!/\.(vue)$/.test(id)) {
+                return null;
             }
-            task = task.then(() => {
-                return crc32(`${id}${line}${column}`).then((hash) => {
-                    result = result.replace('v-ssr', `data-ssr-id="${hash}"`);
-                });
-            });
-        }
-        return {
-            code: result,
-            map: null,
-        };
-    },
-};
+            
+            //首先查找v-ssr所有的位置
+            let ssrIndex = [];
+            let index = src.indexOf('v-ssr');
+            while (index !== -1) {
+                ssrIndex.push(index);
+                index = src.indexOf('v-ssr', index + 1);
+            }
+            
+            //替换v-ssr为data-ssr-id=${hash(file+line+column)}
+            let result = src;
+            for (let index of ssrIndex) {
+                let line = 1;
+                let column = 0;
+                for (let i = 0; i < index; i++) {
+                    if (src[i] === '\n') {
+                        line++;
+                        column = 0;
+                    } else {
+                        column++;
+                    }
+                }
+                const hash = crc32Instance.init().update(`${id}${line}${column}`).digest('hex');
+                result = result.replace('v-ssr', `data-ssr-id="${hash}"`);
+            }
+            return result;
+        },
+    }
+}
 
 
-export function useId(){
+export function useId() {
     const instance = getCurrentInstance();
     return instance.props.dataSsrId;
 }
